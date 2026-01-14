1ee71ab07c542c4fe3421f741d93afc68105a09d
Danish Ali Siddiqui <d.siddiqui@aafaqtech.com>
2026-01-14 15:41:08 +0530

    Fixed rendering issue for better loading time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -23,6 +25,8 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollToTopWrapper />
+      <Analytics />
+      <SpeedInsights />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
